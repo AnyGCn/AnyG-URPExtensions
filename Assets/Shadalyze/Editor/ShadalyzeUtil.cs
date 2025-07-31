fc28950753129dffd9fa05f6b438233a4fe8001d
yuningguo <yuningguo@tencent.com>
2025-07-31 16:44:21 +0800

    Initial Shadalyze module.

---
+++ Assets/Shadalyze/Editor/ShadalyzeUtil.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+
+namespace Shadalyze.Editor
+{
+    public static class ShadalyzeUtil
+    {
+        [MenuItem("Assets/Shadalyze/Compile")]
+        public static void CompileShader()
+        {
+            
+        }
+        
+    }
+}
