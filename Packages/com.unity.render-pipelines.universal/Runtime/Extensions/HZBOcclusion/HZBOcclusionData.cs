775235ab795c837bca73a890d235653153fce799
yuningguo <yuningguo@tencent.com>
2025-10-16 20:03:16 +0800

    Add HZB occlusion module.

---
+++ Packages/com.unity.render-pipelines.universal/Runtime/Extensions/HZBOcclusion/HZBOcclusionData.cs
@@ -0,0 +1,13 @@
+namespace UnityEngine.Rendering.Universal
+{
+    public class HZBOcclusionData
+    {
+        public Camera camera;
+        public int lastRenderFrameCount = -1;
+        
+        public HZBOcclusionData(Camera camera)
+        {
+            
+        }
+    }
+}
